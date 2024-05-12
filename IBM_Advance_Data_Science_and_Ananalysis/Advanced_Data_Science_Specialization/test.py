055cd15fde4dce4b1b3a1634dc475705956d70d6
mehmetkadri1 <78965971+mehmetkadri1@users.noreply.github.com>
2024-05-12 14:04:58 +0100

    Add files via upload

---
+++ IBM_Advance_Data_Science_and_Ananalysis/Advanced_Data_Science_Specialization/test.py
@@ -0,0 +1,15 @@
+from pyspark import SparkContext
+
+# Initialize SparkContext
+sc = SparkContext("local", "test")
+rdd = sc.parallesize(range(100))
+sum = rdd.sum()
+n =rdd.count()
+mean = sum/n
+print (mean) 
+rdd.sortBy(lambda x : x).zipWithIndex().map(lambda (value,key) : (key, value)).collect()
+n = sortedAndIndexed.count()
+if (n % 2 == 1) :
+    print ('odd case')
+else:
+    print ('even case')
